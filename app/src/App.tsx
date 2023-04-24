ec2d865d334af0a5ea9c8cae7612f461b179c2c5
Jakub Szczepanek <kubaszczepanek@interia.pl>
2023-04-24 11:17:52 +0200

    login page fix

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -38,7 +38,6 @@ export default function App() {
   if (!session) {
     return (
         <div style={{width: '100vw', height: '100vh', backgroundColor: Colors.BG}}>
-          <h1 color={'white'}>{getRedirectURL()}</h1>
             <div style={{borderRadius: 10, borderStyle: 'solid', borderColor: Colors.Accent, borderWidth: 1, maxWidth: 300, height: 410, padding: '2em', position: 'absolute', left: 0, right: 0, top: 0, bottom: 0, margin: 'auto'}}>
               <Auth
                 supabaseClient={supabase}
