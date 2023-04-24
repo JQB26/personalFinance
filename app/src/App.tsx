362dc99a671facf18036650a4b211a27e95a0e50
Jakub Szczepanek <kubaszczepanek@interia.pl>
2023-04-24 11:16:58 +0200

    login redirects, font update

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,11 +1,15 @@
 import './index.css'
-import React, { useState, useEffect } from 'react'
-import { createClient } from '@supabase/supabase-js'
-import { Auth } from '@supabase/auth-ui-react'
-import { ThemeSupa } from '@supabase/auth-ui-shared'
+import React, {useEffect, useState} from 'react'
+import {createClient} from '@supabase/supabase-js'
+import {Auth} from '@supabase/auth-ui-react'
+import {ThemeSupa} from '@supabase/auth-ui-shared'
 import Home from "./home/Home";
 import {QueryClient, QueryClientProvider} from "react-query";
 import {Colors} from "./shared/colors";
+
+const getRedirectURL = () => {
+  return process.env.REACT_APP_NODE_ENV === 'dev' ? 'http://localhost:3000/' : 'https://personal-finance-psi.vercel.app/';
+};
 
 const supabaseUrl = 'https://hnttufbkxsdmupulzxwp.supabase.co' // process.env.REACT_APP_SUPABASE_URL
 const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImhudHR1ZmJreHNkbXVwdWx6eHdwIiwicm9sZSI6ImFub24iLCJpYXQiOjE2ODEzMjY2MzEsImV4cCI6MTk5NjkwMjYzMX0.T5tKHHY6sapvnqgXITPTaMmHN0ZnyzWY9qc0xbNorUg' // process.env.REACT_APP_SUPABASE_KEY
@@ -34,11 +38,13 @@ export default function App() {
   if (!session) {
     return (
         <div style={{width: '100vw', height: '100vh', backgroundColor: Colors.BG}}>
+          <h1 color={'white'}>{getRedirectURL()}</h1>
             <div style={{borderRadius: 10, borderStyle: 'solid', borderColor: Colors.Accent, borderWidth: 1, maxWidth: 300, height: 410, padding: '2em', position: 'absolute', left: 0, right: 0, top: 0, bottom: 0, margin: 'auto'}}>
               <Auth
                 supabaseClient={supabase}
                 appearance={{ theme: ThemeSupa }}
                 providers={['google']}
+                redirectTo={getRedirectURL()}
                 theme="dark"
               />
             </div>
